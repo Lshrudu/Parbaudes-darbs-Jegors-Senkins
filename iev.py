1713e81e253afced2c7ae701964db9a566fce629
Lshrudu <142120412+Lshrudu@users.noreply.github.com>
2026-03-04 14:37:49 +0200

    Uzrakstīta funkciju aprēķini

--- iev.py
+++ iev.py
@@ -3,3 +3,8 @@ def Ievade():
       x=int(float("Ievadi abola daudzumu(kg): "))
     y=int(float("Ievadi cukura cenu(euro): "))
     return (x, y)
+
+def aprekini(x, y):
+    cuk_daudz=x/2
+    cēna=y*cuk_daudz
+    
