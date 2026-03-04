7455ee33c38e39f4e4053ea0e40154f1340267ae
Lshrudu <142120412+Lshrudu@users.noreply.github.com>
2026-03-04 14:38:52 +0200

    Izveidots fails ar funkciju nosaukumiem

--- iev.py
+++ iev.py
@@ -1,4 +1,4 @@
-.
+
 def Ievade():
       x=int(float("Ievadi abola daudzumu(kg): "))
     y=int(float("Ievadi cukura cenu(euro): "))
@@ -7,4 +7,6 @@ def Ievade():
 def aprekini(x, y):
     cuk_daudz=x/2
     cēna=y*cuk_daudz
+
+def
     
